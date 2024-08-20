da2f64523bc79d70fbc5f2674c0680727962c3f2
StimzRx <103589496+StimzRx@users.noreply.github.com>
2024-08-20 03:28:31 -0500

    Fixed no launch

--- images/minecraft/scripts/startup.sh
+++ images/minecraft/scripts/startup.sh
@@ -2,7 +2,10 @@
 set -e
 
 source /opt/gow/bash-lib/utils.sh
+source /opt/gow/launch-comp.sh
 
 gow_log "Minecraft Prism Launcher startup.sh"
 
 gow_log "Starting Prism Launcher..."
+
+launcher /Applications/prism_launcher.AppImage --appimage-extract-and-run
